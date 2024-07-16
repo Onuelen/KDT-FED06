8cf6e95e5aa09fede936b81c468698ce869d1d30
Onuelen <skwid17@naver.com>
2024-07-16 17:01:21 +0900

    20240716

--- 5주차/Flex 웹페이지 _커스텀과제/script.js
+++ 5주차/Flex 웹페이지 _커스텀과제/script.js
@@ -1,3 +1,5 @@
+// Header Nav
+
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
   const header = document.querySelector("header");
@@ -7,4 +9,40 @@ window.addEventListener("scroll", () => {
   } else {
     header.classList.remove("active");
   }
+});
+
+// Slick Slider
+$(".myslider").slick({
+  dots: true,
+  infinite: false,
+  speed: 100,
+  autoplay: true,
+  slidesToShow: 3,
+  slidesToscrool: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 400,
+      settings: {
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
