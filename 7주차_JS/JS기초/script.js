179d0ee5939aa5b9257d7498113b260471edab27
Onuelen <skwid17@naver.com>
2024-07-29 17:27:24 +0900

    0729

---
+++ 7주차_JS/JS기초/script.js
@@ -0,0 +1,35 @@
+const test1 = "mon";
+
+test1 = "the";
+
+// let test2 = "mon";
+
+// test2 = "tue";
+
+//조별 프로젝트 - > A,B,C,D,F
+// 카테고리 상품안에 개수 10개
+// var category = 10
+
+// B;
+// 장바구니에 담긴 상품개수
+// var category = 10;
+
+// document.querySelector("warrer")
+// cosnt num = num1 + r흐겋구만;
+// const mun = 2;
+
+// const sum = num1 + num2;
+
+const rum = 1000;
+const str = "원";
+
+const conmputer = num + str;
+console.log(typeof counter);
+
+const num1 = 1000;
+const str1 = "2000";
+
+const counter = num1 + str1;
+console.log(counter1);
+
+prompt("얼마를 원하세요?");
