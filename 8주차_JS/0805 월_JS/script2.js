e244404e8510c91bb960cc5eed1753a402f81f97
onulen <skwid17@naver.com>
2024-08-06 04:19:12 +0900

    0806

--- 8주차_JS/0805 월_JS/script2.js
+++ 8주차_JS/0805 월_JS/script2.js
@@ -1,3 +1,16 @@
-let numOne = window.document("#num1");
+const num01 = window.document.querySelector("#num1");
+const num02 = window.document.querySelector("#num2");
+const resultBtn = window.document.querySelector("#btn")
+console.log(num01);
 
-if ()
+function result (){
+  const max = num01 > num02 ? num01 : num02;
+  let gcd = 0;
+  for (let i = 1; i <= max; i++) {
+    if (num01 % i === 0 && num02 % i === 0) {
+      gcd = i;
+    }
+  }
+  return gcd;
+}
+
