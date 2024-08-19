12e9b8140b75ad21075a6076f7cb5f971f5d2a54
Onuelen <skwid17@naver.com>
2024-08-19 18:09:28 +0900

    0819 kdtfedjsj

---
+++ 10주차_JS/0819_월_03_ES6/script2.js
@@ -0,0 +1,25 @@
+// JS ES6
+// map & set
+// 배열 & 객체
+// 배열 : 인덱스 // 개수 // 넣고, 빼고
+// 객체 : 프로퍼티 형태 (*어떤 자료가 무슨 의미인지)
+// 배열 + 객체 => 합성체 => Map
+// Map => 배열의 형태를 띄고 있는 이터러블한 객체의 자료구조들의 공통적인 약점!!!
+// 중복되는 값을 제어X
+
+// const bag = new Map();
+// bag.set("color", "red");
+
+// console.log(bag);
+
+const myCup = new Map([
+  ["color", "white"],
+  ["material", "ceramic"],
+  ["capacity", "300ml"],
+]);
+
+console.log(myCup);
+
+myCup.set("type", "mini");
+
+console.log(myCup);
