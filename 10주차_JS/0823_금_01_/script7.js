e5d5031ab011449b98ea63a236ff04d17d179975
seongjun1208 <skwid17@naver.com>
2024-08-23 14:02:45 +0900

    0823 kdt fed

---
+++ 10주차_JS/0823_금_01_/script7.js
@@ -0,0 +1,22 @@
+// const displayHello = () => {
+//   console.log("Hello");
+// };
+
+// const displayHello = async () => {
+//   console.log("Hello");
+// };
+
+// displayHello();
+
+// console.log(displayHello());
+
+// const whatsYourFavorite = () => {
+//   const fav = "Javascript";
+//   return new Promise((resolve, reject) => resolve(fav));
+// };
+
+// const displaySubject = (subject) => {
+//   return new Promise((resolve, reject) => resolve(`Hello,${subject}`));
+// };
+
+// whatsYourFavorite().then(displaySubject).then(console.log);
