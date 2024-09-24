fbf3ae0057f713a262e396d58fab29e9b55d7dab
seongjun1208 <skwid17@naver.com>
2024-09-24 16:36:49 +0900

    0924JSJ react+

---
+++ 15주차/sns-flatform/src/components/tstest.tsx
@@ -0,0 +1,9 @@
+// import { log } from "console";
+
+// let num: number = 10;
+
+// // num = "hello";
+
+// const fnc = (a, b) => {
+//   console.log("hello");
+// };
---
+++ 15주차/sns-flatform/src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBccMJ5jdZqpDKl-yzdBof0It8AD47WE0",
+  authDomain: "sns-flatform01.firebaseapp.com",
+  projectId: "sns-flatform01",
+  storageBucket: "sns-flatform01.appspot.com",
+  messagingSenderId: "1054215415363",
+  appId: "1:1054215415363:web:5a63e091984d5eedd496f6",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
