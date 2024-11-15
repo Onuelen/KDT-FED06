623ec71ff34c12d132fd88e81b6e83508705bbf9
seongjun1208 <skwid17@naver.com>
2024-11-15 14:30:14 +0900

    1115 update

--- 22주차/241113/ui-main/src/pages/book/[id].tsx
+++ 22주차/241113/ui-main/src/pages/book/[id].tsx
@@ -2,6 +2,9 @@ import React from "react";
 // import { useRouter } from "next/router";
 import style from "./[id].module.css";
 import { title } from "process";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchOneBook from "@/lib/fetch-one-book";
 
 const mockData = {
   id: 1,
@@ -15,10 +18,23 @@ const mockData = {
     "https://shopping-phinf.pstatic.net/main_3888828/38888282618.20230913071643.jpg",
 };
 
-const Index = () => {
-  const { id, title, subTitle, description, author, publisher, coverImgUrl } =
-    mockData;
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const id = context.params!.id;
+  const book = await fetchOneBook(Number(id));
+  return {
+    props: { book },
+  };
+};
 
+const Index = ({
+  book,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  if (!book) return "문제가 발생했습니다! 다시 시도해주세요!!";
+
+  const { id, title, subTitle, description, author, publisher, coverImgUrl } =
+    book;
   return (
     <div className={style.container}>
       <div
